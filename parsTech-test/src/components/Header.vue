3c51dd15a1b3027f270e594c74eae852a0671caf
mohammadjavad.ebrahimi <mohammadjavad.ebrahimi@partdp.ir>
2023-05-30 16:14:11 +0430

    feat():add dark mode

--- parsTech-test/src/components/Header.vue
+++ parsTech-test/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light" :class="{'bg-dark':!islightMode}">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" :class="{'navbar-dark bg-dark':!islightMode}" >
     <a  @click="togglelightMode()" class="navbar-brand" href="#">Dark/Light</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
