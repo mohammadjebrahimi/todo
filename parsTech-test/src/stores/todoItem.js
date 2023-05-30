efb663bbd0099e3048d1b4c49ab12ceb92c33095
mohammadjavad.ebrahimi <mohammadjavad.ebrahimi@partdp.ir>
2023-05-30 14:56:51 +0430

    refactor: make teo store compatible to one component

---
+++ parsTech-test/src/stores/todoItem.js
@@ -0,0 +1,150 @@
+import { computed, ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useTodoItemStore = defineStore('todoItem', () => {
+  const parentId = ref('ascacsscacacacs')
+  const Id = ref('todoItem')
+  const rows = ref({
+    ascacsscacacacs:
+      [{
+        title: 'jfgjf',
+        description: 'ssfsf',
+        dueDate: 'sfsfss',
+        priority: 'dggsg',
+        creationDate: 's'
+
+      }, {
+        title: 'jfgjffff',
+        description: 'sdgfgdsfsf',
+        dueDate: 'sfsfdfgdfgss',
+        priority: 'dggdfgdfgdfgsg',
+        creationDate: 's'
+      }],
+    fhfhfdh: [],
+    fhhgjghjghjfhfdh: []
+  })
+  const filter = ref({
+    title: '',
+    description: '',
+    dueDate: '',
+    priority: '',
+    creationDate: ''
+  })
+  const search = ref({
+    selectedColumn: 'title',
+    searchString: ''
+  })
+  const headers = ref({
+    title: { isVisible: true, isSearchable: true },
+    description: { isVisible: true, isSearchable: true },
+    dueDate: { isVisible: true, isSearchable: false },
+    priority: { isVisible: true, isSearchable: false },
+    creationDate: { isVisible: true, isSearchable: false }
+  })
+
+
+
+
+  const filterOption = computed(() => {
+    console.log({ rows });
+    return rows.value[parentId.value].reduce((acc, item) => {
+      for (const key in headers.value) {
+        if (item[key] !== undefined) {
+          acc[key] = new Set([...acc[key] ?? [], item[key]]);
+        }
+      }
+      return acc
+    }, {})
+  })
+
+
+
+
+
+
+  const searchedRows = ({ rows }) => {
+
+    return rows[parentId.value].filter((item) => {
+      const column = search.value.selectedColumn
+      if (item[column] === undefined || !item[column].includes(search.value.searchString))
+        return false;
+
+      return true;
+    })
+  }
+  const filteredRows = (rows) => rows.filter((item) => {
+    for (const key in filter.value) {
+      if (item[key] === undefined || item[key] != filter.value[key] && filter.value[key])
+        return false;
+    }
+    return true;
+  })
+  const updateFilter = (filters) => {
+    for (const key in filters) {
+      if (filter.value[key] !== undefined)
+        filter.value[key] = filters[key]
+    }
+
+  }
+  const addRow = (row) => {
+    const creationDate = new Date().toLocaleDateString();
+    rows.value[parentId.value].push({ ...row, creationDate })
+  };
+  const deleteRow = (index) => {
+    console.log(rows.value[parentId.value][0]);
+    return checkRowIndex(index) ? rows.value[parentId.value].splice(index, 1) : []
+  }
+  const updateRow = ({ index, row }) => {
+
+    for (const key in row) {
+      if (rows.value[parentId.value][index][key] !== undefined)
+        rows.value[parentId.value][index][key] = row[key]
+    }
+    console.log(rows);
+  }
+  const checkRowIndex = (i) => {
+    const index = rows.value[parentId.value].length > i;
+    return index
+  }
+
+  const updateHeaderVisibility = ({ key, isVisible }) => {
+    return headers.value[key].isVisible = isVisible
+  }
+  const updateSearch = (searchOption) => {
+    for (const key in searchOption) {
+      if (search.value[key] !== undefined)
+        search.value[key] = searchOption[key]
+    }
+  }
+
+  const setParentId = (id) => {
+    parentId.value = id
+  }
+  const addKeyByParentId = (id) => {
+    if (!rows.value[id])
+      rows.value[id] = []
+  }
+  const deleteKeyByParentId = (id) => {
+    if (rows.value[id])
+      delete rows.value[id]
+
+  }
+  return {
+    rows,
+    addRow,
+    deleteRow,
+    updateRow,
+    headers,
+    updateHeaderVisibility,
+    filteredRows,
+    filter,
+    filterOption,
+    searchedRows,
+    updateFilter,
+    search,
+    updateSearch,
+    setParentId,
+    addKeyByParentId,
+    deleteKeyByParentId
+  }
+})
--- parsTech-test/src/stores/todolist.js
+++ parsTech-test/src/stores/todolist.js
@@ -1,27 +1,33 @@
 import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
+import { useTodoItemStore } from '@/stores/todoItem.js'
+
+
 
 export const useTodoListStore = defineStore('todoList', () => {
-
-  const todoListRows = ref([{
+  const { deleteKeyByParentId ,addKeyByParentId } = useTodoItemStore()
+  const Id = ref('todoList')
+  const rows = ref([{
     title: 'sd',
     description: 'dsg',
-    creationDate: 's'
+    creationDate: 's',
+    id:'ascacsscacacacs'
   }, {
     title: 'dg',
     description: 'dgsdsdgsddgd',
-    creationDate: 's'
+    creationDate: 's',
+    id:'fhfhfdh'
   }])
-  const todoListFilter = ref({
+  const filter = ref({
     title: '',
     description: '',
-    creationDate: ''
+    creationDate: '',
   })
-  const todoListSearch = ref({
+  const search = ref({
     selectedColumn: 'title',
     searchString: ''
   })
-  const todoListHeaders = ref({
+  const headers = ref({
     title: { isVisible: true, isSearchable: true },
     description: {
       isVisible: true, isSearchable: true
@@ -32,8 +38,8 @@ export const useTodoListStore = defineStore('todoList', () => {
 
 
 
-  const filterOption = computed(() => todoListRows.value.reduce((acc, item) => {
-    for (const key in todoListHeaders.value) {
+  const filterOption = computed(() => rows.value.reduce((acc, item) => {
+    for (const key in headers.value) {
       if (item[key] !== undefined) {
         acc[key] = new Set([...acc[key] ?? [], item[key]]);
       }
@@ -42,74 +48,72 @@ export const useTodoListStore = defineStore('todoList', () => {
   }, {}))
 
 
-
-
-
-
   const searchedRows = ({ rows }) => rows.filter((item) => {
-    const column = todoListSearch.value.selectedColumn
-    if (item[column] === undefined || !item[column].includes(todoListSearch.value.searchString))
+    const column = search.value.selectedColumn
+    if (item[column] === undefined || !item[column].includes(search.value.searchString))
       return false;
 
     return true;
   })
   const filteredRows = (rows) => rows.filter((item) => {
-    for (const key in todoListFilter.value) {
-      if (item[key] === undefined || item[key] != todoListFilter.value[key] && todoListFilter.value[key])
+    for (const key in filter.value) {
+      if (item[key] === undefined || item[key] != filter.value[key] && filter.value[key])
         return false;
     }
     return true;
   })
-  const updateTodoListFilter = (filters) => {
+  const updateFilter = (filters) => {
     for (const key in filters) {
-      if (todoListFilter.value[key] !== undefined)
-        todoListFilter.value[key] = filters[key]
+      if (filter.value[key] !== undefined)
+        filter.value[key] = filters[key]
     }
 
   }
   const addRow = (row) => {
     const creationDate = new Date().toLocaleDateString();
-    todoListRows.value.push({ ...row, creationDate })
+    const id = Math.random().toString(16).slice(2)
+    addKeyByParentId(id)
+    rows.value.push({ ...row, creationDate })
   };
   const deleteRow = (index) => {
-    return checkRowIndex ? todoListRows.value.splice(index, 1) : []
+    deleteKeyByParentId(rows.value[index])
+    return checkRowIndex(index) ? rows.value.splice(index, 1) : []
   }
   const updateRow = ({ index, row }) => {
  
     for (const key in row) {
       console.log(row);
-      if (todoListRows.value[index][key] !== undefined)
-      todoListRows.value[index][key] = row[key]
+      if (rows.value[index][key] !== undefined)
+      rows.value[index][key] = row[key]
     }
-console.log(todoListRows);
   }
-  const checkRowIndex = () => {
-    const index = todoListRows.value.indexOf(5);
-    return index > -1
+  const checkRowIndex = (i) => {
+    const index = rows.value.length>= i ;
+    return index 
   }
 
-  const updateTodoListHeaderVisibility = ({ key, isVisible }) => {
-    return todoListHeaders.value[key].isVisible = isVisible
+  const updateHeaderVisibility = ({ key, isVisible }) => {
+    return headers.value[key].isVisible = isVisible
   }
-  const updateTodoListSearch = (searchOption) => {
+  const updateSearch = (searchOption) => {
     for (const key in searchOption) {
-      if (todoListSearch.value[key] !== undefined)
-        todoListSearch.value[key] = searchOption[key]
+      if (search.value[key] !== undefined)
+        search.value[key] = searchOption[key]
     }
   }
   return {
-    todoListRows,
+    rows,
     addRow,
     deleteRow,
     updateRow,
-    todoListHeaders,
-    updateTodoListHeaderVisibility,
+    headers,
+    updateHeaderVisibility,
     filteredRows,
-    todoListFilter,
+    filter,
     filterOption,
     searchedRows,
-    updateTodoListFilter,
-    todoListSearch,
-    updateTodoListSearch
+    updateFilter,
+     search,
+     updateSearch
   }
 })
